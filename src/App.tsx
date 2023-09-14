40f05a8ee8ac25bc4805b18e1622ea97f1f75db7
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-09-14 15:49:56 +0900

    Test checkbox components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react'
 import Global from './css/reset'
 import './App.css'
 
+import { type Item } from 'stories/inputs/CheckboxGroup'
+import CheckboxGroup from 'stories/inputs/CheckboxGroup'
+
 function App() {
+  const items = [
+    {
+      value: 'children1',
+      children: 'children1',
+      disabled: false,
+    },
+    {
+      value: 'children2',
+      children: 'children2',
+      disabled: false,
+    },
+    {
+      value: 'children3',
+      children: 'children3',
+      disabled: false,
+    },
+  ]
+
+  const [selectedItems, setSelectedItems] = useState<Item[]>([])
+
   return (
     <>
       <Global />
+      <CheckboxGroup
+        items={items}
+        selectedItems={selectedItems}
+        onChange={setSelectedItems}
+      />
+      <p>{`Check된 Items: ${selectedItems.map(({ value }) => value).join(' ')}`}</p>
     </>
   )
 }
