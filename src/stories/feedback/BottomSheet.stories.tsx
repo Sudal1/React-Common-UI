0cebb8dcbaf7ba4615c40f374f0e310b0dd30d4a
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-09-25 14:51:19 +0900

    Update BottomSheet stories

--- src/stories/feedback/BottomSheet.stories.tsx
+++ src/stories/feedback/BottomSheet.stories.tsx
@@ -1,42 +1,71 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
 import { RecoilRoot } from 'recoil'
+import { useBottomSheetActions } from 'states/bottomSheet'
+import Button from 'stories/inputs/Button'
 import BottomSheet from './BottomSheet'
 
 interface BottomSheetItem {
   name: string
-  onClick(): void
+  onClick?(): void
 }
 
 const meta = {
   title: 'Global/BottomSheet',
   component: BottomSheet,
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
   },
   tags: ['autodocs'],
-  decorators: [(story) => <RecoilRoot>{story()}</RecoilRoot>],
 } satisfies Meta<typeof BottomSheet>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-const defaultItems: BottomSheetItem[] = [
-  { name: 'Bottom Sheet 1', onClick: () => alert('1') },
-  { name: 'Bottom Sheet 2', onClick: () => alert('2') },
-  { name: 'Bottom Sheet 3', onClick: () => alert('3') },
-  { name: 'Bottom Sheet 4', onClick: () => alert('4') },
-]
+const Template: Story = {
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <div style={{ minHeight: '250px', padding: '2.4rem' }}>
+          <Story />
+        </div>
+      </RecoilRoot>
+    ),
+  ],
+  render: ({ visible, items }) => {
+    const [_, updateArgs] = useArgs<{ visible: boolean; items: BottomSheetItem[] }>()
+    const { open, close } = useBottomSheetActions()
 
-export const Default: Story = {
+    const dummyItems = [
+      { name: 'Bottom Sheet 1', onClick: () => alert('1') },
+      { name: 'Bottom Sheet 2', onClick: () => alert('2') },
+      { name: 'Bottom Sheet 3', onClick: () => alert('3') },
+      { name: 'Bottom Sheet 4', onClick: () => alert('4') },
+    ]
+
+    const handleClose = () => {
+      close()
+      updateArgs({ visible: !visible })
+    }
+
+    const handleOpen = () => {
+      open(dummyItems)
+      updateArgs({ visible: !visible, items: dummyItems })
+    }
+
+    return (
+      <>
+        <Button onClick={handleOpen}>Click !</Button>
+        <BottomSheet visible={visible} items={items} onClose={handleClose} />
+      </>
+    )
+  },
   args: {
     visible: false,
-    items: defaultItems,
+    items: [{ name: 'Bottom Sheet 1', onClick: () => alert('1') }],
   },
 }
 
 export const Visible: Story = {
-  args: {
-    visible: true,
-    items: defaultItems,
-  },
+  ...Template,
 }
